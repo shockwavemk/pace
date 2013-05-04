004d1b5a65b132fd1b5f424eb8774d60274ce876
Martin Kramer <martinkr@mail.uni-paderborn.de>
2013-05-04 18:43:37 +0200

    Rename of Main Forms

--- PaceClient/MainClientForm.cs
+++ PaceClient/MainClientForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PaceClient
 {
-    public partial class Form1 : Form
+    public partial class MainClientForm : Form
     {
-        public Form1()
+        public MainClientForm()
         {
             InitializeComponent();
         }
--- PaceServer/MainServerForm.cs
+++ PaceServer/MainServerForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PaceServer
 {
-    public partial class Form1 : Form
+    public partial class MainServerForm : Form
     {
-        public Form1()
+        public MainServerForm()
         {
             InitializeComponent();
         }
