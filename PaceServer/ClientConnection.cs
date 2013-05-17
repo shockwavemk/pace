a8fea921de20cc1a44991d8ea9069a05d826b772
Martin Kramer <martinkr@mail.uni-paderborn.de>
2013-05-17 20:42:45 +0200

    some Code for Connectionpart on Server

--- PaceServer/ClientConnection.cs
+++ PaceServer/ClientConnection.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PaceServer
@@ -10,5 +12,51 @@ namespace PaceServer
     class ClientConnection
     {
         public TcpClient TcpClient;
+        private Thread ThreadConnection;
+        private bool _connectionEstablished;
+
+        public ClientConnection(TcpClient tcpConnection)
+        {
+            TcpClient = tcpConnection;
+            ThreadConnection = new Thread(AcceptClient);
+            ThreadConnection.Start();
+        }
+
+        public void AcceptClient()
+        {
+            try
+            {
+                ClientReceiver = new StreamReader(TcpClient.GetStream());
+                ClientSender = new StreamWriter(TcpClient.GetStream());
+
+                // TODO Registration / Identification
+                bool Register = false; // TODO
+                int ClientId = 0; // TODO
+
+                if (Register)
+                {
+                    CloseConnection();
+                    return;
+                }
+                else
+                {
+                    _connectionEstablished = true;
+                    ClientInformation clientInformation = ClientInformation.GetClientInformation(ClientId);
+
+                }
+
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public void CloseConnection()
+        {
+            TcpClient.Close();
+            ThreadConnection.Abort();
+        }
     }
 }
